94a70fcc4f626b0e9896e98bb4594e0164e0ba73
N0il <sergio.r.gama@gmail.com>
2021-11-18 15:58:14 +0000

    finished organizing js file

--- src/dynamics.js
+++ src/dynamics.js
@@ -103,24 +103,29 @@ setTimeout(function(){
 },2900);
 */
 
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DONE UNTIL HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Cavities and Seeds Settings
-var configs = document.getElementById("button-settings-info--config");
-configs.addEventListener("click", cavitiesConfig);
+var configsButton = document.getElementById("button-settings-info--config");
+configsButton.addEventListener("click", gameplayAreaConfigs);
+
+function gameplayAreaConfigs(){
+  cavitiesConfig();
+  seedsConfig();
+}
 
 function cavitiesConfig(){
-  var cavities = document.getElementsByClassName("d-game-background-cavity");
-  var cavitiesTop = document.getElementsByClassName("d-gameplay-cavity");
+  var backgroundCavities = document.getElementsByClassName("d-game-background-cavity");
+  var gameplayCavities = document.getElementsByClassName("d-gameplay-cavity");
 
-  while(cavities[0]){
-    cavities[0].remove();
-    cavitiesTop[0].remove();
+  while(backgroundCavities[0]){
+    backgroundCavities[0].remove();
+    gameplayCavities[0].remove();
   }
 
   var quantities = document.getElementsByClassName("input-settings-info--quantities");
+  var nCavities = quantities[0].value;
 
-  for(i = 0; i < quantities[0].value; i++){
+  for(i = 0; i < nCavities; i++){
+    //Game Background
     var cavitie = document.createElement("div");
     cavitie.classList.add("d-game-background-cavity");
     document.getElementById("d-game-background-cavities-p1").appendChild(cavitie);
@@ -129,7 +134,7 @@ function cavitiesConfig(){
     cavitie2.classList.add("d-game-background-cavity");
     document.getElementById("d-game-background-cavities-p2").appendChild(cavitie2);
 
-    //Top layer
+    //Gameplay Area (Top of background)
     var cavitieTop = document.createElement("div");
     cavitieTop.classList.add("d-gameplay-cavity");
     document.getElementById("d-gameplay-cavities-p1").appendChild(cavitieTop);
@@ -138,23 +143,17 @@ function cavitiesConfig(){
     cavitieTop2.classList.add("d-gameplay-cavity");
     document.getElementById("d-gameplay-cavities-p2").appendChild(cavitieTop2);
   }
-
-  seedsConfig();
 }
 
 var seedRes = ["../res/seeds/seed_red.png", "../res/seeds/seed_green.png", "../res/seeds/seed_yellow.png", "../res/seeds/seed_blue.png"];
 var seedResAlt = ["../res/seeds/seed_red_alt.png", "../res/seeds/seed_green_alt.png", "../res/seeds/seed_yellow_alt.png", "../res/seeds/seed_blue_alt.png"];
 
-function randomSeedRes(){
-  // Returns a random integer from 0 to 3:
+function randomSeedResource(){
+  //returns a random integer from 0 to 3:
   var randNum = Math.ceil(Math.random() * 4) - 1;
 
-    // Returns a random integer from 0 to 1:
+  //returns a random integer from 0 to 1:
   var altShape = Math.ceil(Math.random() * 2) - 1;
-
-  console.log("alt: " + altShape);
-
-  console.log(randNum);
 
   if(altShape){
     return seedResAlt[randNum];
@@ -163,75 +162,62 @@ function randomSeedRes(){
   }
 }
 
-//returns in percentage
 function randSeedPositionTop(){
   var randPos = Math.random() * 65 + 10;
-
   return randPos + "%";
 }
 
-//returns in percentage
 function randSeedPositionLeft(){
   var randPos = Math.random() * 55 + 10;
-
   return randPos + "%";
 }
-
-//connect seeds number with configs
 
 function seedsConfig(){
   var quantities = document.getElementsByClassName("input-settings-info--quantities");
   var cavs = document.getElementsByClassName("d-gameplay-cavity");
 
-  //var cavsBot = document.getElementsByClassName("d-game-background-cavity");
-
   var seeds = document.getElementsByClassName("img-seed");
-  
-  //var seedsBack = document.getElementsByClassName("img-seed-back");
-
-  while(seeds[0]){
-    seeds[0].remove();
-    //seedsBack[0].remove();
-  }
-
   var seedQuantities = document.getElementsByClassName("p-seed-quantitie");
 
+  //removing all seeds
+  while(seeds[0]){
+    seeds[0].remove();
+  }
+
+  //removing all seed quantities
   while(seedQuantities[0]){
     seedQuantities[0].remove();
   }
 
-  for(i = 0; i < quantities[0].value * 2;i++){
-    for(j = 0; j < quantities[1].value; j++){
+  var nCavities = quantities[0].value;
+  var nSeeds = quantities[1].value;
+
+  for(i = 0; i < nCavities * 2; i++){
+    //adding seeds
+    for(j = 0; j < nSeeds; j++){
       var seed = document.createElement("img");
       seed.classList.add("img-seed");
       seed.style.top =  randSeedPositionTop();
       seed.style.left = randSeedPositionLeft();
-      seed.src = randomSeedRes();
+      seed.src = randomSeedResource();
       cavs[i].appendChild(seed);
     }
-    
+
+    //adding seed quantities (hover): one to each cavity 
     var seedQuan = document.createElement("p");
     seedQuan.classList.add("p-seed-quantitie");
     seedQuan.innerHTML += cavs[i].childElementCount;
     cavs[i].appendChild(seedQuan);
   }
 
-  for(i = 0; i < 2;i++){
-  var storages = document.getElementsByClassName("d-gameplay-storage");
-  var seedQuanS = document.createElement("p");
-    seedQuanS.classList.add("p-seed-quantitie");
-    seedQuanS.innerHTML += storages[i].childElementCount;
-    storages[i].appendChild(seedQuanS);
+  //adding seed quantities (hover): one to each storage
+  for(i = 0; i < 2; i++){
+    var storages = document.getElementsByClassName("d-gameplay-storage");
+    var seedStorageQuantities = document.createElement("p");
+    seedStorageQuantities.classList.add("p-seed-quantitie");
+    seedStorageQuantities.innerHTML += storages[i].childElementCount;
+    storages[i].appendChild(seedStorageQuantities);
   }
-/*
-  for(i = 0; i < quantities[0].value * 2;i++){
-    for(j = 0; j < quantities[1].value; j++){
-      var seed2 = document.createElement("img");
-      seed2.classList.add("img-seed-back");
-      cavsBot[i].appendChild(seed2);
-    }
-  }
-  */
 }
 
 // Reset Cavities and Seeds
@@ -244,18 +230,10 @@ function reset(){
   quantities[0].value = 6;
   quantities[1].value = 5;
 
-  cavitiesConfig();
+  gameplayAreaConfigs();
 }
 
-
-
-
-
-
-
-
-
-
+//TODO IMPLEMENTATION OF MVC? OR STATE PATTERN? or BOTH?
 /*
 class Cavity {
   constructor() {}
