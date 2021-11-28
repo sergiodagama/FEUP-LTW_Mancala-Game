1fdf6ea1242f9360b973baf5bfce7495292fec28
N0il <sergio.r.gama@gmail.com>
2021-11-28 22:54:52 +0000

    continued game dev in Model section only

--- src/scripts/Model/StorageModel.js
+++ src/scripts/Model/StorageModel.js
@@ -1,7 +1,7 @@
 import "CavityModel.js"
 
 class StorageModel extends CavityModel{  //one storage is a cavity with special properties
-    constructor(nSeeds = 0) {
+    constructor(nSeeds) {
         super(nSeeds);
     }
 }
